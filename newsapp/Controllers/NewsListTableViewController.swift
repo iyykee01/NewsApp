ad0ffdd44acc1b81fbc96b9edf896b8b17f1fa7d
iyykee01 <iyykee01@gmail.com>
2021-07-02 17:31:22 +0100

    Add architecture for app (MVVM)

---
+++ newsapp/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewsListTableViewController.swift
+//  newsapp
+//
+//  Created by chukwuma.udokporo on 02/07/2021.
+//
+
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
